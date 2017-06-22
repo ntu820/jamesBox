beeb7899bcdfd4c7e4968cf93e58e4f9a52c0428
ntu820 <ntu_zhouyang@163.com>
2017-06-22 15:35:08 +0800

    新的系统

---
+++ 任务项目/任务管理系统.pc/files/pc/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r],s,_(t,u,v,w,x,y,z,_(),A,_(B,C,D,E,F,_(G,H,I,J),K,null,L,E,M,E,N,O,P,null,Q,R,S,T,U,V,W,R),X,_(),Y,_(),Z,_(ba,[])),bb,_(),bc,_());}; 
+var b="url",c="pc.html",d="generationDate",e=new Date(1488959357717.89),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="NewVariable",o="keys",p="ClickOnHandle",q="StarRatingState",r="scroll",s="page",t="packageId",u="d3e3ab9594924c0e893cb1fa322f5709",v="type",w="Axure:Page",x="name",y="PC",z="notes",A="style",B="baseStyle",C="627587b6038d43cca051c114ac41ad32",D="pageAlignment",E="near",F="fill",G="fillType",H="solid",I="color",J=0xFFFFFFFF,K="image",L="imageHorizontalAlignment",M="imageVerticalAlignment",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="应用字体",W="borderWidth",X="adaptiveStyles",Y="interactionMap",Z="diagram",ba="objects",bb="masters",bc="objectPaths";
+return _creator();
+})());
---
+++ 任务项目/任务管理系统.pc/files/任务管理系统/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r],s,_(t,u,v,w,x,y,z,_(),A,_(B,C,D,E,F,_(G,H,I,J),K,null,L,E,M,E,N,O,P,null,Q,R,S,T,U,V,W,R),X,_(),Y,_(),Z,_(ba,[])),bb,_(),bc,_());}; 
+var b="url",c="任务管理系统.html",d="generationDate",e=new Date(1488959357644.89),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="NewVariable",o="keys",p="ClickOnHandle",q="StarRatingState",r="scroll",s="page",t="packageId",u="155df5f85494465fa10dac0ba495ff64",v="type",w="Axure:Page",x="name",y="任务管理系统",z="notes",A="style",B="baseStyle",C="627587b6038d43cca051c114ac41ad32",D="pageAlignment",E="near",F="fill",G="fillType",H="solid",I="color",J=0xFFFFFFFF,K="image",L="imageHorizontalAlignment",M="imageVerticalAlignment",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="应用字体",W="borderWidth",X="adaptiveStyles",Y="interactionMap",Z="diagram",ba="objects",bb="masters",bc="objectPaths";
+return _creator();
+})());
